48524cb403f6eef4d0625d95f883c08dcb7ee929
paige <Paige_Hensley@jabil.com>
2025-01-16 19:51:50 -0500

    Update to run.py logging

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from app.routes import routes
 
 app = Flask(__name__)
+
+# Register blueprints or routes
 app.register_blueprint(routes)
 
+
+@app.route("/")
+def index():
+    return "Slack Bot is running!", 200
+
+
 if __name__ == "__main__":
-    app.run(port=3000)
+    app.run(debug=True)
